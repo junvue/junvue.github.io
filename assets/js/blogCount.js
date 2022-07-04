41695fa4aa96d5dc982faf18e809d7ffabf2e32f
Junvue <junny970@gmail.com>
2022-07-04 11:50:42 +0900

    add blog pageview #3

--- assets/js/blogCount.js
+++ assets/js/blogCount.js
@@ -6,115 +6,156 @@
  *   - countUpBlog.js <https://github.com/inorganik/countUpBlog.js>
  */
 
-  const getInitStatusBlog = (function () {
-    let hasInit = false;
-    return () => {
-      let ret = hasInit;
-      if (!hasInit) {
-        hasInit = true;
-      }
-      return ret;
-    };
-  }());
+const getInitStatusBlog = (function () {
+  let today_hasInit = false;
+  let total_hasInit = false;
 
-  const BlogPvOpts = (function () {
-    function getContent(selector) {
-      return $(selector).attr("content");
-    }
+  return {
+    status(type) {
+      if(type == "today") {
+        let ret = today_hasInit;
   
-    function hasContent(selector) {
-      let content = getContent(selector);
-      return (typeof content !== "undefined" && content !== false);
-    }
-
-    const todayURL = "https://junvue-blog.du.r.appspot.com/query?id=ag12fmp1bnZ1ZS1ibG9nchULEghBcGlRdWVyeRiAgICYpteACgw";
-    const totalURL = "https://junvue-blog.du.r.appspot.com/query?id=ag12fmp1bnZ1ZS1ibG9nchULEghBcGlRdWVyeRiAgICYpteACQw";
-  
-    return {
-      getProxyMeta(type) {
-        if(type == "today")
-            return todayURL;
-        else(type == "total")
-            return totalURL;
-      },
-      getLocalMeta() {
-        return getContent("meta[name=pv-cache-path]");
-      },
-      hasLocalMeta() {
-        return hasContent("meta[name=pv-cache-path]");
-      }
-    };
-  
-  }());
-  
-  const blogPvStorage = (function () {
-    const Keys = {
-      KEY_PV: "blog-pv",
-      KEY_PV_SRC: "blog-pv_src",
-      KEY_CREATION: "blog-pv_created_date"
-    };
-  
-    const Source = {
-      LOCAL: "same-origin",
-      PROXY: "cors"
-    };
-  
-    function get(key) {
-      return localStorage.getItem(key);
-    }
-  
-    function set(key, val) {
-      localStorage.setItem(key, val);
-    }
-  
-    function saveCache(pv, src) {
-      set(Keys.KEY_PV, pv);
-      set(Keys.KEY_PV_SRC, src);
-      set(Keys.KEY_CREATION, new Date().toJSON());
-    }
-  
-    return {
-      keysCount() {
-        return Object.keys(Keys).length;
-      },
-      hasCache() {
-        return (localStorage.getItem(Keys.KEY_PV) !== null);
-      },
-      getCache() {
-        return JSON.parse(localStorage.getItem(Keys.KEY_PV));
-      },
-      saveLocalCache(pv) {
-        saveCache(pv, Source.LOCAL);
-      },
-      saveProxyCache(pv) {
-        saveCache(pv, Source.PROXY);
-      },
-      isExpired() {
-        let date = new Date(get(Keys.KEY_CREATION));
-        date.setHours(date.getHours() + 1);   // per hour
-        return Date.now() >= date.getTime();
-      },
-      isFromLocal() {
-        return get(Keys.KEY_PV_SRC) === Source.LOCAL;
-      },
-      isFromProxy() {
-        return get(Keys.KEY_PV_SRC) === Source.PROXY;
-      },
-      newerThan(pv) {
-        return blogPvStorage.getCache().totalsForAllResults["ga:pageviews"] > pv.totalsForAllResults["ga:pageviews"];
-      },
-      inspectKeys() {
-        if (localStorage.length !== blogPvStorage.keysCount()) {
-          localStorage.clear();
-          return;
+        if (!today_hasInit) {
+          today_hasInit = true;
         }
   
-        for(let i = 0; i < localStorage.length; i++){
+        return ret;
+      } else if(type == "total") {
+        let ret = total_hasInit;
+  
+        if (!total_hasInit) {
+          total_hasInit = true;
+        }
+  
+        return ret;
+      }
+    }
+  }
+}());
+
+function getAPI_URL(type) {
+  const todayURL = "https://junvue-blog.du.r.appspot.com/query?id=ag12fmp1bnZ1ZS1ibG9nchULEghBcGlRdWVyeRiAgICYpteACgw";
+  const totalURL = "https://junvue-blog.du.r.appspot.com/query?id=ag12fmp1bnZ1ZS1ibG9nchULEghBcGlRdWVyeRiAgICYpteACQw";
+
+  if (type == "today")
+    return todayURL;
+  else (type == "total")
+    return totalURL;
+}
+
+const blogPvStorage = (function () {
+  const today_Keys = {
+    KEY_PV: "today-pv",
+    KEY_PV_SRC: "today-pv_src",
+    KEY_CREATION: "today-pv_created_date"
+  };
+
+  const total_Keys = {
+    KEY_PV: "total-pv",
+    KEY_PV_SRC: "total-pv_src",
+    KEY_CREATION: "total-pv_created_date"
+  };
+
+  const Source = {
+    LOCAL: "same-origin",
+    PROXY: "cors"
+  };
+
+  function get(key) {
+    return localStorage.getItem(key);
+  }
+
+  function set(key, val) {
+    localStorage.setItem(key, val);
+  }
+
+  function saveCache(type, pv, src) {
+    if(type == "today") {
+      set(today_Keys.KEY_PV, pv);
+      set(today_Keys.KEY_PV_SRC, src);
+      set(today_Keys.KEY_CREATION, new Date().toJSON());
+    } else if(type == "total") {
+      set(total_Keys.KEY_PV, pv);
+      set(total_Keys.KEY_PV_SRC, src);
+      set(total_Keys.KEY_CREATION, new Date().toJSON());
+    }
+  }
+
+  return {
+    keysCount(type) {
+      if(type == "today")
+        return Object.keys(today_Keys).length;
+      else if(type == "total")
+        return Object.keys(total_Keys).length;
+    },
+    hasCache(type) {
+      if(type == "today")
+        return (localStorage.getItem(today_Keys.KEY_PV) !== null);
+      else if(type == "total")
+        return (localStorage.getItem(total_Keys.KEY_PV) !== null);
+    },
+    getCache(type) {
+      if(type == "today")
+        return JSON.parse(localStorage.getItem(today_Keys.KEY_PV));
+      else if(type == "total")
+        return JSON.parse(localStorage.getItem(total_Keys.KEY_PV));
+    },
+    saveProxyCache(type, pv) {
+      saveCache(type, pv, Source.PROXY);
+    },
+    isExpired(type) {
+      if(type == "today") {
+        let date = new Date(get(today_Keys.KEY_CREATION));
+
+        date.setHours(date.getHours() + 1);   // per hour
+        return Date.now() >= date.getTime();
+      } else if(type == "total") {
+        let date = new Date(get(total_Keys.KEY_CREATION));
+
+        date.setHours(date.getHours() + 1);   // per hour
+        return Date.now() >= date.getTime();
+      }
+    },
+    isFromLocal(type) {
+      if(type == "today")
+        return get(today_Keys.KEY_PV_SRC) === Source.LOCAL;
+      else if(type == "total")
+        return get(total_Keys.KEY_PV_SRC) === Source.LOCAL;
+    },
+    isFromProxy(type) {
+      if(type == "today")
+        return get(today_Keys.KEY_PV_SRC) === Source.PROXY;
+      else if(type == "total")
+        return get(total_Keys.KEY_PV_SRC) === Source.PROXY;
+    },
+    newerThan(type, pv) {
+      return blogPvStorage.getCache(type).totalsForAllResults["ga:pageviews"] > pv.totalsForAllResults["ga:pageviews"];
+    },
+    inspectKeys(type) {
+      if (localStorage.length !== blogPvStorage.keysCount(type)) {
+        localStorage.clear();
+        return;
+      }
+      if(type == "today") {
+        for (let i = 0; i < localStorage.length; i++) {
           const key = localStorage.key(i);
           switch (key) {
-            case Keys.KEY_PV:
-            case Keys.KEY_PV_SRC:
-            case Keys.KEY_CREATION:
+            case today_Keys.KEY_PV:
+            case today_Keys.KEY_PV_SRC:
+            case today_Keys.KEY_CREATION:
+              break;
+            default:
+              localStorage.clear();
+              return;
+          }
+        }
+      } else if(type == "total") {
+        for (let i = 0; i < localStorage.length; i++) {
+          const key = localStorage.key(i);
+          switch (key) {
+            case total_Keys.KEY_PV:
+            case total_Keys.KEY_PV_SRC:
+            case total_Keys.KEY_CREATION:
               break;
             default:
               localStorage.clear();
@@ -122,127 +163,155 @@
           }
         }
       }
-    };
-  }()); /* blogPvStorage */
-  
-  function countUpBlog(min, max, destId) {
-    if (min < max) {
-      let numAnim = new countUpBlog(destId, min, max);
+    }
+  };
+}()); /* blogPvStorage */
 
-      if (!numAnim.error) {
-        numAnim.start();
-      } else {
-        console.error(numAnim.error);
-      }
-    }
-  }
-  
-  function countBlogPV(rows) {
-    let count = 0;
-  
-    if (typeof rows !== "undefined" ) {
-        const temp = rows[0][0];
-        
-        count += temp;
-    }
-  
-    return count;
-  }
-  
-  function tackleBlogPV(rows, elem, hasInit) {
-    let count = countBlogPV(rows);
-    count = (count === 0 ? 1 : count);
-  
-    if (!hasInit) {
-      elem.text(new Intl.NumberFormat().format(count));
+function countUpBlog(min, max, destId) {
+  if (min < max) {
+    let numAnim = new countUpBlog(destId, min, max);
+
+    if (!numAnim.error) {
+      numAnim.start();
     } else {
-      const initCount = parseInt(elem.text().replace(/,/g, ""), 10);
-
-      if (count > initCount) {
-        countUpBlog(initCount, count, elem.attr("id"));
-      }
+      console.error(numAnim.error);
     }
   }
-  
-  function displayBlogPageviews(data) {
-    if (typeof data === "undefined") {
-      return;
-    }
-  
-    let hasInit = getInitStatusBlog();
+}
 
-    const rows = data.rows;
+function countBlogPV(rows) {
+  let count = 0;
 
-    console.log(rows);
-  
-    tackleBlogPV(rows, $("#todayPV"), hasInit);
+  if (typeof rows !== "undefined") {
+    const temp = parseInt(rows[0][0], 10);
+
+    count += temp;
   }
+
+  return count;
+}
+
+function tackleBlogPV(type, rows, elem, hasInit) {
+  let count = countBlogPV(rows);
+  count = (count === 0 ? 1 : count); // if count is '0' then replace to '1' or just 'count'
+
+  console.log("[" + type + "] hasInit is " + hasInit)
+
+  if(!hasInit) {
+    console.log("[" + type + "] InitStatusBlog = no");
+    elem.text(new Intl.NumberFormat().format(count));
+    console.log("[" + type + "] display pageview done.");
+  } else {
+    console.log("[" + type + "] InitStatusBlog = yes");
+    const initCount = parseInt(elem.text().replace(/,/g, ""), 10);
+
+    console.log("[" + type + "] initCount = " + initCount);
+
+    if (count > initCount) {
+      console.log("[" + type + "] count is > initcount");
+      countUpBlog(initCount, count, elem.attr("id"));
+    }
+  }
+}
+
+function displayBlogPageviews(data) {
+  if(typeof data === "undefined") {
+    return;
+  }
+
+  const rows = data.rows;
+
+  let type = data.query["start-date"];
   
-  function fetchProxyBlogPageviews() {
-    $.ajax({
+  if(type == "today") {
+    let hasInit = getInitStatusBlog.status(type);
+
+    console.log("[" + type + "] displayBlogPageviews rows = " + rows[0]);
+    tackleBlogPV(type, rows, $("#todayPV"), hasInit);
+  } else {
+    type = "total";
+
+    let hasInit = getInitStatusBlog.status(type);
+
+    console.log("[" + type + "] displayBlogPageviews rows = " + rows[0]);
+    tackleBlogPV(type, rows, $("#totalPV"), hasInit);
+  }
+}
+
+function fetchProxyBlogPageviews(type, callback) {
+  $.ajax({
     type: "GET",
-    url: BlogPvOpts.getProxyMeta(today),
+    url: getAPI_URL(type),
     dataType: "jsonp",
     jsonpCallback: "displayBlogPageviews",
     success: (data) => {
-        blogPvStorage.saveProxyCache(JSON.stringify(data));
-        console.log("세이브프록시캐쉬햇어용");
+      blogPvStorage.saveProxyCache(type, JSON.stringify(data));
+      console.log("[" + type + "] 2. save ProxyCache");
+      callback(type);
     },
     error: (jqXHR, textStatus, errorThrown) => {
-        console.log("Failed today pageviews: " + errorThrown);
+      console.log("[" + type + "] Failed today pageviews: " + errorThrown);
     }
-    });
-  }
-  
-  /*
-  function fetchLocalBlogPageviews(hasCache = false) {
-    return fetch(BlogPvOpts.getLocalMeta())
-      .then(response => response.json())
-      .then(data => {
-        if (hasCache) {
-          // The cache from the proxy will sometimes be more recent than the local one
-          if (blogPvStorage.isFromProxy() && blogPvStorage.newerThan(data)) {
-            console.log("여기걸리긴하세용?");
-            return;
-          }
-        }
-        displayBlogPageviews(data);
-        blogPvStorage.saveLocalCache(JSON.stringify(data));
-        console.log("세이브로컬캐쉬햇어용");
-      });
-  }
-  */
-  
-  $(function() {  
-    console.log(localStorage.getItem(Keys));
-    blogPvStorage.inspectKeys();
-  
-    if (blogPvStorage.hasCache()) {
-        console.log("갖고있나영?");
-      displayBlogPageviews(blogPvStorage.getCache());
-  
-      if (blogPvStorage.isExpired()) {
-        if (BlogPvOpts.hasLocalMeta()) {
-          fetchLocalBlogPageviews(true).then(fetchProxyBlogPageviews);
-        } else {
-          fetchProxyBlogPageviews();
-        }
-  
-      } else {
-        if (blogPvStorage.isFromLocal()) {
-          fetchProxyBlogPageviews();
-        }
-      }
-    } else { // no cached
-        console.log("않갖고있는데영");
-      if (BlogPvOpts.hasLocalMeta()) {
-        console.log("갖고있나영?2");
-        fetchLocalBlogPageviews().then(fetchProxyBlogPageviews);
-      } else {
-        console.log("않갖고있는데영2");
-        fetchProxyBlogPageviews();
-      }
-    }
-  
   });
-  
+}
+
+function getPageviews(type) {
+  blogPvStorage.inspectKeys(type);
+
+  if (blogPvStorage.hasCache(type)) { // has Cashe
+    console.log("[" + type + "] 1. blogPvStorage.hasCashe = yes");
+    displayBlogPageviews(blogPvStorage.getCache(type));
+
+    if(type == "total") {
+      console.log("[" + type + "] blogCount SUCCESS ! ! !");
+    } else {
+      console.log("[" + type + "] blogCount SUCCESS ! ! !");
+      getPageviews("total");
+    }
+
+    if(blogPvStorage.isExpired(type)) {
+      console.log("[" + type + "] There is expired");
+
+      if(type == "total") {
+        fetchProxyBlogPageviews(type, callback => { console.log("[" + type + "] fetch is done") });
+      } else {
+        fetchProxyBlogPageviews(type, callback => {
+          console.log("[" + type + "] fetch is done");
+          console.log("---------------------------------------------------------------");
+          getPageviews("total");
+        });
+      }
+    } else {
+      console.log("[" + type + "] There is no expired");
+      if (blogPvStorage.isFromLocal(type)) {
+        console.log("[" + type + "] isFromLocal Yes");
+
+        if(type == "total") {
+          fetchProxyBlogPageviews(type, callback => { console.log("[" + type + "] fetch is done") });
+        } else {
+          fetchProxyBlogPageviews(type, callback => {
+            console.log("[" + type + "] fetch is done");
+            console.log("---------------------------------------------------------------");
+            getPageviews("total");
+          });
+        }
+      }
+    }
+  } else { // has not Cashe
+    console.log("[" + type + "] 1. blogPvStorage.hasCashe = no");
+
+    if(type == "total") {
+        fetchProxyBlogPageviews(type, callback => { console.log("[" + type + "] fetch is done") });
+      } else {
+        fetchProxyBlogPageviews(type, callback => {
+          console.log("[" + type + "] fetch is done");
+          console.log("---------------------------------------------------------------");
+          getPageviews("total");
+        });
+      }
+  }
+}
+
+$(function () {
+  getPageviews("today");
+});
